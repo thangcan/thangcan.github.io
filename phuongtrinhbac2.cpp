3aa02f5b40e100fe466af0bbf7d885c1bf4d5ac5
thangcan <thang25.01.1985@gmail.com>
2022-06-30 14:39:17 +0700

    sửa

--- phuongtrinhbac2.cpp
+++ phuongtrinhbac2.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
-include <math.h>
+#include <math.h>
 using namespace std;
 int main()
 {
-    cout << "giải pt bậc 2 : a*x2 + b*x + c = 0" << "\n";
-    int a,b,c;
+    cout << "giai phuong trinh bac 2 : a*x2 + b*x + c = 0" << endl;
+    float a,b,c;
     cout << "a=";
-    cin >> a
+    cin >> a;
     cout << "b=";
     cin >> b;
     cout << "c=";
     cin >> c;
     if(a==0)
     {
-        if(b=0)
+        if(b==0)
         {
-            if(c=0)
+            if(c==0)
             {
-            cout << "pt có vsn";
+            cout << "pt co vo so nghiem";
             }
             else
             {
-                cout << "pt vô nghiệm";
+                cout << "pt vo nghiem";
             }
           
         }
-          else
+        else
             {
-                cout << "pt có nghiệm duy nhất" << "x=" << -b/a << "/n";
+                cout << "pt co nghiem duy nhat x= " << -c/b ;
             }
     }
     
@@ -36,16 +36,16 @@ int main()
                 float delta = b*b - 4*a*c;
                 if(delta<0)
                 {
-                    cout << "pt vô nghiệm";
+                    cout << "pt vo nghiem";
                 }
-                else if(delta==o)
+                else if(delta==0)
                 {
-                    cout << "pt có nghiệm duy nhất x=" << -b/(2*a)<< "/n";
+                    cout << "pt co nghiem duy nhat x=" << -b/(2*a);
                 }
                 else
                 {
-                    cout << "pt có 2 nghiệm phân biệt x1 = "<< (-b+sqrt(delta)/(2*a))<<"/n";
-                    cout << "x2 =" << (-b-sqrt(delta)/(2*a))<<"/n";
+                    cout << "pt co 2 nghiem phan biet x1 = "  << (-b+sqrt(delta)/(2*a))<< " x2 =" << (-b-sqrt(delta)/(2*a));
+                    
                 }
             }   
              
